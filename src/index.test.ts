dbae11eed56db441b0e8aede2fdc21685571966b
Dam Phuong Dong <dongdp.exp@gmail.com>
2025-09-29 10:47:42 +0700

    First clean commit with correct folder

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { sum } from './index';
+
+test('sum adds numbers correctly', () => {
+  expect(sum(2, 3)).toBe(5);
+});
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export function sum(a: number, b: number): number {
+  return a + b;
+}
+
+export function sub(a: number, b: number): number {
+  return a - b;
+}
+
+console.log("Hello TS CI Demo, sum(2,3) =", sum(2, 3));
+
+console.log("Hello TS CI Demo, sum(2,3) =Hello TS CI Demo, sum(2,3) =Hello TS CI Demo, sum(2,3) =Hello TS CI Demo, sum(2,3) =Hello TS CI Demo, sum(2,3) =", sum(2, 3));
